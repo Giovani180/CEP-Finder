21d2957c687b22466c93cd515bb95d02ac855e2e
Giovani180 <giovani.adriana180@gmail.com>
2021-07-07 19:07:01 -0300

    Update cep-finder.py

--- cep-finder.py
+++ cep-finder.py
@@ -1,12 +1,12 @@
-# -*- coding: utf-8 -*-
-
-import requests
-import json
-import telepot
-import random
-import re
-import sys
-import time
+def new_func():
+    # -*- coding: utf-8 -*-
+    import requests
+    import json
+    import telepot
+    import random
+    import re
+    import sys
+    import time
 # Bot Para consultar CEP e IBGE com Telegram API
 # Modo de uso: /cep 04320-040
 # Developers:
@@ -15,31 +15,32 @@ import time
 
 #bot = telepot.Bot()
 
-reload(sys)
-sys.setdefaultencoding('utf8')
 
-def handle(msg):
-    tipomsg, tipochat, chat_id = telepot.glance(msg)
+
+    def handle(msg):
+        tipomsg, tipochat, chat_id = telepot.glance(msg)
     #print(msg)
 
-    command = msg['text'].split(' ')
-    if command[0] == '/cep':
-        cep = command[1]
-        api = "https://viacep.com.br/ws/"+cep+"/json/unicode/" # API JSON
+        command = msg['text'].split(' ')
+        if command[0] == '/cep':
+            cep = command[1]
+            api = "https://viacep.com.br/ws/"+cep+"/json/unicode/" # API JSON
 
-        r = requests.get(api)
-        results = json.loads(r.content)
-        print(results)
+            r = requests.get(api)
+            results = json.loads(r.content)
+            print(results)
 
-        txt = 'Consulta de CEP e IBGE no Telegram:\n{Criado por Dkr e Tesla.}\n\nCep: '+results['cep']+'\nLogradouro: '+results['logradouro']+'\nComplemento: '+results['complemento']+'\nBairro: '+results['bairro']+'\nLocalidade: '+results['localidade']+'\nUF: '+results['uf']+'\nIBGE: '+results['ibge']
-        bot.sendMessage(chat_id, txt) # Envia O resultado
+            txt = 'Consulta de CEP e IBGE no Telegram:\n{Criado por Dkr e Tesla.}\n\nCep: '+results['cep']+'\nLogradouro: '+results['logradouro']+'\nComplemento: '+results['complemento']+'\nBairro: '+results['bairro']+'\nLocalidade: '+results['localidade']+'\nUF: '+results['uf']+'\nIBGE: '+results['ibge']
+            bot.sendMessage(chat_id, txt) # Envia O resultado
 
-TOKEN = ''
+    TOKEN = ''
 
-bot = telepot.Bot('1246235471:AAFEyhP6EaAuDCrO-AJZgr1V8vkIFVjsB0M') # SUA API AQUI
-bot.message_loop(handle)
-print ('Aguarde...')
-print ('Modo de uso: /cep 04320-040')
+    bot = telepot.Bot('1246235471:AAFEyhP6EaAuDCrO-AJZgr1V8vkIFVjsB0M') # SUA API AQUI
+    bot.message_loop(handle)
+    print ('Aguarde...')
+    print ('Modo de uso: /cep 04320-040')
 
-while 1:
-    time.sleep(10)
+    while 1:
+        time.sleep(10)
+
+new_func()
